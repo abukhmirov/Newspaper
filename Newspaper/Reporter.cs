6197aecb59446924a84d1c17fbca2f4b8e56fd1d
abukhmirov <alexbukhmirov@gmail.com>
2023-06-07 13:35:22 -0600

    Reporter

---
+++ Newspaper/Reporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newspaper
+{
+    public class Reporter
+    {
+        public string Name { get; set; }
+        public string Speciality { get; set; }
+        public List<Article> Articles { get; set; }
+
+    }
+}
